f752cfd8d642de980d5253d1434caf8165c9512a
LC-daoxin <457121827@qq.com>
2020-03-11 17:39:03 +0800

    Project initialization

---
+++ src/views/system/dashboard/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import adminDashboard from './admin'
+import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: { adminDashboard, editorDashboard },
+  data () {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+  .dashboard-container {
+    height: 100%;
+  }
+</style>
