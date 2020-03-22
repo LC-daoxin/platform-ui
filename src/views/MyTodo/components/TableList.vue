d05f4e6f220f254730c4a1c8c0030aa4f3e06ca1
FinnXiong <18845330815@163.com>
2020-03-22 16:16:56 +0800

    完成我的系列页面

---
+++ src/views/MyTodo/components/TableList.vue
@@ -0,0 +1,166 @@
+<template>
+  <main>
+    <el-table :height="tableHeight" size="mini" border>
+      <el-table-column
+        v-for="(column, columnIndex) of checkRoute"
+        :key="columnIndex"
+        :label="column.label"
+        :align="column.align"
+      ></el-table-column>
+    </el-table>
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tableHeight: window.innerHeight - this.$parent.$refs.searchBar.$el.offsetHeight - 282,
+      todoColumns: [
+        {
+          prop: '',
+          label: '流水号',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '主题',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '流程名称',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '当前节点',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '申请人',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '申请时间',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '流程图',
+          align: 'center'
+        }
+      ],
+      doneColumns: [
+        {
+          prop: '',
+          label: '流水号',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '主题',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '流程名称',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '操作节点',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '申请人',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '操作时间',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '操作结果',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '流程图',
+          align: 'center'
+        }
+      ],
+      requestColumns: [
+        {
+          prop: '',
+          label: '流水号',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '主题',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '流程名称',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '当前节点',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '状态',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '申请时间',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '流程图',
+          align: 'center'
+        },
+        {
+          prop: '',
+          label: '操作',
+          align: 'center'
+        }
+      ]
+    }
+  },
+  computed: {
+    checkRoute () {
+      const routeName = this.$route.name
+      switch (routeName) {
+        case 'my-todo':
+          return this.todoColumns
+        case 'my-proxy':
+          return []
+        case 'my-done':
+          return this.doneColumns
+        case 'my-request':
+          return this.requestColumns
+        default:
+          return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  main {
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/views/MyTodo/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dashboard-editor-container">
+    <search-bar ref="searchBar"></search-bar>
+    <table-list></table-list>
+    <el-pagination layout="total, sizes, prev, pager, next, jumper" size="mini"></el-pagination>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar'
+import TableList from './components/TableList'
+
+export default {
+  components: {
+    SearchBar,
+    TableList
+  }
+}
+</script>
+
+<style scoped></style>
